8b24fbd310be888f5be7db4a90ada2b0822deaa4
Dhawal Patel <dpatel37@terpmail.umd.edu>
2025-04-05 18:21:53 -0400

    added simple_social_network.py

---
+++ simple_social_network.py
@@ -0,0 +1,14 @@
+""" A simplesocial network for tracking connections between people. """
+
+class Person:
+    """A person in the social network
+    
+        Attributes:
+            name (str): the persons name.
+            connections (set of Person): other people in the social network who 
+                know this person
+    
+    """
+    
+    
+    
